7ec6fe29d4f915a7d3ab6ac69d9c87126639d898
gonzaloflores <gonzalof@hotmail.com>
2023-12-04 17:35:07 -0300

    Versión 3 - Con Carrito

--- js/main.js
+++ js/main.js
@@ -1,181 +1,255 @@
-let productos = [
-	{ 
-		id:1,
-		producto:"Polera",
-		modelo:"Heather",
-		color:"Military Green",
-		diseno:"Ride Your Bike",
-		imagen:"img/disenos/Corona-Bicicleta.png",
-		alt:"Polera diseño Corona Bicicleta",
-		precio:10000
-	},
-	{
-		id:2,
-		producto:"Polera",
-		modelo:"Heather",
-		color:"Military Green",
-		diseno:"E.T. San Cristóbal",
-		imagen:"img/disenos/ET-CRISTOBAL.png",
-		alt:"Polera diseño E.T. San Cristóbal",
-		precio:10000
-	},
-	{
-		id:3,
-		producto:"Polera",
-		modelo:"Heather",
-		color:"Red Jaspe",
-		diseno:"Funicular Santiago",
-		imagen:"img/disenos/Funicular.png",
-		alt:"Polera diseño Funicular Bellavista",
-		precio:10000
-	},
-	{ 
-		id:4,
-		producto:"Polera",
-		modelo:"Heather Royal",
-		color:"Azul Marino Jaspe",
-		diseno:"I was in Santiago de Chile",
-		imagen:"img/disenos/I-Was-In.png",
-		alt:"Polera diseño I Was In Chile",
-		precio:10000
-	},
-	{ 
-		id:5,
-		producto:"Polera",
-		modelo:"Heather Royal",
-		color:"Granito Jaspe",
-		diseno:"Cerro Manquehue",
-		imagen:"img/disenos/Manquehe.png",
-		alt:"Cerro Manquehue",
-		precio:12000
-	},
-	{ 
-		id:6,
-		producto:"Polera",
-		modelo:"infantil",
-		color:"Blanca",
-		diseno:"Polera diseño Metro Bus",
-		imagen:"img/disenos/Metro-Bus.png",
-		alt:"Polera diseño Liebre Bus",
-		precio:10000
-	},
-	{ 
-		id:7,
-		producto:"Polera",
-		modelo:"Teleferico Sky",
-		color:"Mostaza Jaspe",
-		diseno:"I was in Santiago de Chile",
-		imagen:"img/disenos/Sky-change-colors-final.png",
-		alt:"Polera diseño Teleferico Sky",
-		precio:10000
-	},
-	{ 
-		id:8,
-		producto:"Polera",
-		modelo:"Heather Royal",
-		color:"Azul Marino Jaspe",
-		diseno:"Polera diseño Teleférico de Santiago",
-		imagen:"img/disenos/Teleferico-Digital.png",
-		alt:"Polera diseño Teleférico de Santiago",
-		precio:13000
-	}
-	];
+import productos from './productos.js';
 
-//función con dos argumentos para agregar contenido al HTML
-function agregarHtml (divId, contenido) {
-	var miDiv = document.getElementById(divId);
-	miDiv.insertAdjacentHTML('beforeend', contenido);
+
+//-----------------------------función con dos argumentos para agregar contenido HTML al documento
+function agregarHtml(divId, contenido) {
+    let miDiv = document.getElementById(divId);
+    miDiv.innerHTML = ""; //Limpiar el contenedor
+    miDiv.insertAdjacentHTML('beforeend', contenido);
+	return miDiv;
 }
 
-//Función Que presenta los productos en Pantalla
-function protuctosEnPantalla(array) {
-    let counter = 0;
-    array.some(element => {
-        if (counter < 4) {
-            let card = `<div class='card'><img src='${element.imagen}' alt='${element.alt}'><div class='info'><p>Modelo: ${element.modelo}</p><p>Color: ${element.color}</p><p>Diseño: ${element.diseno}</p><p>Precio $${element.precio}</p><p>ID: ${element.id}</p></div>`;
-            agregarHtml('productos_banner',card);
-            counter++;
-        } else {
-            return true; // Termina el bucle después de imprimir cuatro tarjetas
-        }
+//---------------------Función que da formato CARD HTML a cada producto y presenta los productos en Pantalla.
+function productosEnPantalla(array) {
+    const cards = array.map(element => {
+        return `<div class="card">
+				<img src="${element.imagen}" alt="${element.alt}">
+				<div class="info">
+					<p>Modelo: ${element.modelo}</p>
+					<p>Color: ${element.color}</p>
+					<p>Diseño: ${element.diseno}</p>
+					<p>Precio $${element.precio}</p>
+					<button class="boton-comprar" data-id="${element.id}">COMPRAR</button>
+				</div>
+			</div>`;
     });
+    const cardsHTML = cards.join(''); // Une los strings en un solo string
+    agregarHtml('productos_banner', cardsHTML);
 }
 
 //Función para mezclar las propiedades del Array
 function ArrayMix(array) {
-	const Array_Mix = array;	// Crear una copia del array original
+    const Array_Mix = array; // Crear una copia del array original
     for (let i = Array_Mix.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));	// Obtener un índice aleatorio
-        [Array_Mix [i], Array_Mix [j]] = [Array_Mix [j], Array_Mix [i]]; // Intercambiar elementos
+        const j = Math.floor(Math.random() * (i + 1)); // Obtener un índice aleatorio
+        [Array_Mix[i], Array_Mix[j]] = [Array_Mix[j], Array_Mix[i]]; // Intercambiar elementos
     }
 
     return Array_Mix;
 }
 
-//Función flecha que solicita nombre al cliente y valida
-let validarNombre = () => {
-    let nombre = prompt("Por favor ingresar su nombre");
-    nombre = nombre.trim();
-    let regExp = /\d/; //La expresión regular /\d/ verifica si una cadena contiene al menos un dígito (0-9).
-    let tieneNumeros = regExp.test(nombre);
-    while (nombre === "" || tieneNumeros ){
-        if(nombre === ""){ alert("Nombre no ingresado");}else{ alert("Nombre ingresado con numeros");}
-        nombre = prompt("Por favor ingresar su nombre correctamente y sin numeros");
-        nombre = nombre.trim();
-        tieneNumeros = regExp.test(nombre);
-    } 
-    return nombre;
-};
+//-------------------------------Funcion que toma la busqueda desde un input y va haciendo una busqueda incremental
+// Variable para almacenar los productos filtrados
+let productosFiltrados = []; 
 
-//Funcion seleccionar color de polera y validar la entrada del cliente
-function colorPolera(Array){
-	let polera_color = false;
-	let color;
+function inicializarFiltro() {
+    const inputBusqueda = document.getElementById("Buscador");
 
-	while(polera_color === false){
-		color = parseInt(prompt("¿Que color de Polera desea ver? \n 1) Military Green \n 2) Red Jaspe \n 3) Azul Marino Jaspe \n 4) Granito Jaspe \n 5) Blanca \n 6) Mostaza Jaspe \n 7) Todos "));
-		if((typeof color)==="number" && (color > 0 && color <= 7) ){
-			switch (color){
-				case 1: polera_color = Array.filter((polera)=>{return polera.color.includes('Green')});
-					alert("Usted selecciono el color Green");
-				break;
-				case 2: polera_color = Array.filter((polera)=>{return polera.color.includes('Red')});
-					alert("Usted selecciono el color Red");
-				break;
-				case 3: polera_color = Array.filter((polera)=>{return polera.color.includes('Azul')});
-					alert("Usted selecciono el color Azul");
-				break;
-				case 4: polera_color = Array.filter((polera)=>{return polera.color.includes('Granito')});
-					alert("Usted selecciono el color Granito");
-				break;
-				case 5: polera_color = Array.filter((polera)=>{return polera.color.includes('Blanca')});
-					alert("Usted selecciono el color Blanco");
-				break;
-				case 6: polera_color = Array.filter((polera)=>{return polera.color.includes('Mostaza')});
-					alert("Usted selecciono el color Mostaza");
-				break;
-				case 7: polera_color = Array;
-					alert("Usted desea ver todas las poleras");
-				break;
-			}
-		} else {
-			alert("Debe escribir un numero del 1 al 7 segundo el color elegido"); 
-			polera_color = false;		
-		} 
-	}
-	return polera_color;
+    inputBusqueda.addEventListener("input", function () {
+        const textoDeBusqueda = inputBusqueda.value.toLowerCase();
+
+        // Si no hay texto en el campo de búsqueda, mostrar todos los productos
+        if (textoDeBusqueda === '') {
+            productosEnPantalla(productos);
+            productosFiltrados = [];
+            return;
+        }
+
+        // Filtrar incrementalmente solo los nuevos productos que coincidan con el filtro previo
+        const nuevosProductosFiltrados = productosFiltrados.length > 0
+            ? productosFiltrados.filter(producto => producto.diseno.toLowerCase().includes(textoDeBusqueda))
+            : productos.filter(producto => producto.diseno.toLowerCase().includes(textoDeBusqueda));
+
+        productosEnPantalla(nuevosProductosFiltrados);
+        productosFiltrados = nuevosProductosFiltrados; // Actualizar el estado del filtro
+    });
 }
 
-//Solicitar Nombre al cliente
-agregarHtml('login',validarNombre());
+//-------------------------------------------------Funciones de sesión de usuario
+function getCookieValue(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return decodeURIComponent(cookie.substring(name.length + 1));
+        }
+    }
+    return null;
+}
 
-//Consultar al cliente y ordenar aleatoriamente los productos seleccionados
-let produtos_desordenando = ArrayMix(colorPolera(productos));
+function setCookie(name, value, days) {
+    let expires = '';
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = `; expires=${date.toUTCString()}`;
+    }
+    document.cookie = `${name}=${encodeURIComponent(value)}${expires}; path=/`;
+}
 
-//Exponer los productos en pantalla
-protuctosEnPantalla(produtos_desordenando);
+function deleteCookie(name) {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+}
+
+function login() {
+    const userName = 'userName';
+    const miDiv = document.getElementById("login");
+
+    const existingCookie = getCookieValue(userName);
+
+    if (existingCookie) {
+        const nombreUsuario = existingCookie.replace("%20", " ");
+        const contenidoLogin = `<p>${nombreUsuario}</p> | <a href="#" id="cerrarSesion">Cerrar Sesión</a>`;
+        miDiv.innerHTML = contenidoLogin;
+
+        const parrafoCerrarSesion = document.getElementById("cerrarSesion");
+        parrafoCerrarSesion.addEventListener('click', () => {
+            deleteCookie(userName);
+            miDiv.innerHTML = "";
+            const loginForm = `<form>
+                                    <input type="text" name="nombre" placeholder="Iniciar Sesión">
+                                    <input type="submit" value="Iniciar Sesión">
+                                </form>`;
+            miDiv.innerHTML = loginForm;
+        });
+    } else {
+        miDiv.innerHTML = "";
+        const loginForm = `<form>
+                                <input type="text" name="nombre" placeholder="Iniciar Sesión">
+                                <input type="submit" value="Iniciar Sesión">
+                            </form>`;
+        miDiv.innerHTML = loginForm;
+
+        const form = document.querySelector('form');
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            const nombre = event.target.elements.nombre.value;
+            setCookie(userName, nombre, 7); // 7 days expiration, you can modify this value
+            login(); // Update the UI after setting the cookie
+        });
+    }
+}
+
+//-----------------------------------------------Funciones del Carrito
+ // Obtener referencias a elementos del DOM
+const BotonAbrirModal = document.getElementById('abrirModal');
+const divModal = document.getElementById('modal');
+const closeBtn = document.querySelector('.modal__cerrar');
+
+// Función para abrir el modal
+function abrirModal(e) {
+    e.preventDefault();
+    divModal.style.display = 'block';
+}
+
+// Función para cerrar el modal
+function closeCartModal(e) {
+    e.preventDefault();
+    divModal.style.display = 'none';
+}
+
+// Event listeners para abrir y cerrar el modal
+BotonAbrirModal.addEventListener('click', abrirModal);
+closeBtn.addEventListener('click', closeCartModal);
+
+// Event listener para cerrar el modal haciendo click fuera de él
+window.addEventListener('click', (event) => {
+    if (event.target === divModal) {
+        closeCartModal();
+    }
+});
+
+/////////////////////////////////////////////////////////
+// Función para obtener el carrito del LocalStorage
+function verificarCarritoLocalStorage () {
+    let carrito = [];
+    if(localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'));
+    } 
+    return carrito;
+}
 
 
+let boton_vaciar_carrito = document.getElementById("borrar_productos");
+boton_vaciar_carrito.addEventListener("click",(e) => {
+    e.preventDefault;
+    carrito = [];
+    localStorage.removeItem('carrito');
+    agregarCantidadProductos();
+    cargarModal(); });
 
+function cantidadProductos(){
+    return carrito.length;
+}
 
+function agregarCantidadProductos(){
+    let iconCarrito = document.getElementById("header_total_productos");
+    let modal__totaProductos = document.getElementById("total-productos");
+    iconCarrito.innerHTML = "";
+    iconCarrito.innerHTML = cantidadProductos();
+    modal__totaProductos.innerHTML = "";
+    modal__totaProductos.innerHTML = cantidadProductos();
 
+}
+
+// Función para guardar el carrito en el LocalStorage
+function guardarCarritoEnLocalStorage() {
+    localStorage.getItem('carrito') && localStorage.removeItem('carrito');
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+// Función para calcular el total del carrito
+function calcularTotalCarrito() {
+    return carrito.reduce((total, producto) => total + producto.precio * producto.cantidad, 0);
+}
+
+//Función que limpia el HTML del Modal y lo carga con productos del Carrito actual
+function cargarModal () {
+    const itemsCarrito = document.getElementById('items-carrito');
+    // Limpiar el contenido antes de cargar los productos
+    itemsCarrito.innerHTML = ''; 
+
+    carrito.forEach(producto => {
+        const nuevoItem = document.createElement('li');
+        nuevoItem.textContent = `${producto.diseno} - $${producto.precio.toFixed(2)} - Cantidad: ${producto.cantidad}`;
+        itemsCarrito.appendChild(nuevoItem); 
+        agregarCantidadProductos();
+    });
+
+    total = calcularTotalCarrito();
+    const totalCarrito = document.getElementById('total-carrito');
+    totalCarrito.textContent = `$${total.toFixed(2)}`;
+}
+
+// Función para agregar un producto al carrito
+function agregarProductoAlCarrito(producto) {
+    carrito.push(producto);
+    guardarCarritoEnLocalStorage();
+}
+
+// Función para agregar evento a los botones de compra
+function botonProductoAgregar() {
+    const botonesProducto = document.getElementsByClassName("boton-comprar");
+    [...botonesProducto].forEach((boton) => {
+        boton.addEventListener("click", () => {
+            const idProducto = boton.dataset.id;
+            const productoSeleccionado = productos.find((producto) => producto.id === parseInt(idProducto));
+            agregarProductoAlCarrito(productoSeleccionado);
+            // Actualizar visualmente el carrito
+            cargarModal(); 
+        });
+    });
+}
+
+let carrito = verificarCarritoLocalStorage();
+let total = 0;
+
+cargarModal();
+
+login();
+
+productosEnPantalla(ArrayMix(productos));
+
+botonProductoAgregar();
+
+inicializarFiltro();
---
+++ js/productos.js
@@ -0,0 +1,92 @@
+let productos = [
+	{ 
+		id:1,
+		producto:"Polera",
+		modelo:"Heather",
+		color:"Military Green",
+		diseno:"Ride Your Bike",
+		imagen:"img/disenos/Corona-Bicicleta.png",
+		alt:"Polera diseño Corona Bicicleta",
+		precio:10000,
+		cantidad:1
+	},
+	{
+		id:2,
+		producto:"Polera",
+		modelo:"Heather",
+		color:"Military Green",
+		diseno:"E.T. San Cristóbal",
+		imagen:"img/disenos/ET-CRISTOBAL.png",
+		alt:"Polera diseño E.T. San Cristóbal",
+		precio:12000,
+		cantidad:1
+	},
+	{
+		id:3,
+		producto:"Polera",
+		modelo:"Heather",
+		color:"Red Jaspe",
+		diseno:"Funicular Santiago",
+		imagen:"img/disenos/Funicular.png",
+		alt:"Polera diseño Funicular Bellavista",
+		precio:14000,
+		cantidad:1
+	},
+	{ 
+		id:4,
+		producto:"Polera",
+		modelo:"Heather Royal",
+		color:"Azul Marino Jaspe",
+		diseno:"I was in Santiago de Chile",
+		imagen:"img/disenos/I-Was-In.png",
+		alt:"Polera diseño I Was In Chile",
+		precio:9000,
+		cantidad:1
+	},
+	{ 
+		id:5,
+		producto:"Polera",
+		modelo:"Heather Royal",
+		color:"Granito Jaspe",
+		diseno:"Cerro Manquehue",
+		imagen:"img/disenos/Manquehe.png",
+		alt:"Cerro Manquehue",
+		precio:15000,
+		cantidad:1
+	},
+	{ 
+		id:6,
+		producto:"Polera",
+		modelo:"infantil",
+		color:"Blanca",
+		diseno:"Polera diseño Metro Bus",
+		imagen:"img/disenos/Metro-Bus.png",
+		alt:"Polera diseño Liebre Bus",
+		precio:8500,
+		cantidad:1
+	},
+	{ 
+		id:7,
+		producto:"Polera",
+		modelo:"Teleferico Sky",
+		color:"Mostaza Jaspe",
+		diseno:"I was in Santiago de Chile",
+		imagen:"img/disenos/Sky-change-colors-final.png",
+		alt:"Polera diseño Teleferico Sky",
+		precio:9500,
+		cantidad:1
+	},
+	{ 
+		id:8,
+		producto:"Polera",
+		modelo:"Heather Royal",
+		color:"Azul Marino Jaspe",
+		diseno:"Polera diseño Teleférico de Santiago",
+		imagen:"img/disenos/Teleferico-Digital.png",
+		alt:"Polera diseño Teleférico de Santiago",
+		precio:13000,
+		cantidad:1
+	}
+	];
+
+export default productos;
